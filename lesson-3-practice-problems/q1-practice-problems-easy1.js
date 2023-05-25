f6b7483b90c17b7d17d41e42396f44f726f0a731
Bobby <simplyhappydev@gmail.com>
2023-05-24 19:27:42 -0500

    adding in some more clarification

--- lesson-3-practice-problems/q1-practice-problems-easy1.js
+++ lesson-3-practice-problems/q1-practice-problems-easy1.js
@@ -1,19 +1,28 @@
 // Question 1
 // Will the code below raise an error?
-
+// Bonus:
 let numbers = [1, 2, 3];
 numbers[6] = 5;
+//Bonus👇
+numbers[4]; // what will this line return? My Answer => it returns the Primitive Value undefined
 
 // [ 1, 2, 3, <3 empty items>, 5 ]
-// My answer: No, it will add the Primitive Number value 5 at index 6. I believe it'll also add undefined to any empty index values without any current elements up to that index.
+// My answer: No, it will add the ////Primitive Number////
+// it will add EMPTY VALUES to the array where there are no elements up to the inserted index. BUT the empty values will return/log 'undefined'
 
 console.log(numbers[4] === undefined); // Will log 'true', because accessing uninitialized index returns 'undefined'
 console.log(numbers[4] === null); // Will log 'false', because the slot does not contain the value 'null'
 console.log(4 in numbers); // Will log 'false', because the index '4' is not initialized
+/*
+the <in> operator is used to check if a property exists in an object. Since arrays in JavaScript are a type of object, where the keys are the index positions, you can use the in operator to check if a certain index exists in the array.
+
+The expression 4 in numbers checks whether the array numbers has an element at index 4. The in operator will return true if the index exists in the array, and false if it does not.
+*/
 
 console.log(`numbers array: ` + numbers); //=> numbers array: 1,2,3,,,,5
 console.log(`value at index 0: ` + numbers[0]); //=> value at index 0: 1
 console.log(`value at index 3: ` + numbers[3]); // value at index 3: undefined
+console.log(`value at index 4: ` + numbers[4]); // value at index 4: undefined
 console.log(`value at index 6: ` + numbers[6]); // value at index 6: 5
 
 /*
@@ -31,4 +40,11 @@ console.log(numbers[4] === undefined); // Will log 'true', because accessing uni
 console.log(numbers[4] === null); // Will log 'false', because the slot does not contain the value 'null'
 console.log(4 in numbers); // Will log 'false', because the index '4' is not initialized
 
+
+Remember, in checks for the existence of the index (or property, in case of non-array objects), not the value of the element at that index. This is why it returns true for empty slots in an array.
+
+You can also use in to check if properties exist in non-array objects:
+let obj = { 'key1': 'value1', 'key2': 'value2' };
+console.log('key1' in obj);  // Logs 'true'
+console.log('key3' in obj);  // Logs 'false'
 */

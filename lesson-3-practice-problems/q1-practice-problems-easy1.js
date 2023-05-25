08f166dae864574017154177bada65592c142f96
Bobby <simplyhappydev@gmail.com>
2023-05-24 19:32:53 -0500

    Adding in LS answer

--- lesson-3-practice-problems/q1-practice-problems-easy1.js
+++ lesson-3-practice-problems/q1-practice-problems-easy1.js
@@ -10,6 +10,14 @@ numbers[4]; // what will this line return? My Answer => it returns the Primitive
 // My answer: No, it will add the ////Primitive Number////
 // it will add EMPTY VALUES to the array where there are no elements up to the inserted index. BUT the empty values will return/log 'undefined'
 
+/** Launch school answer: It doesn't raise an error. The JavaScript engine will treat array slots 3 through 5 as empty slots. 
+ * Bonus Answer:
+numbers[4] will output undefined, but the slot is empty -- it doesn't have a value, not even undefined, in spite of what the line returns. This distinction is important: if you use map(), for instance, the new array will also have empty slots in the corresponding positions. On the other hand, slots with a value of undefined will be remapped based on the return value of the callback:
+ */
+
+// numbers[5] = undefined; // => [ 1, 2, 3, <2 empty items>, undefined, 5 ]
+console.log(`numbers.map(() => 10)) logs:` + numbers.map(() => 10)); // => [ 10, 10, 10, <2 empty items>, 10, 10 ]
+
 console.log(numbers[4] === undefined); // Will log 'true', because accessing uninitialized index returns 'undefined'
 console.log(numbers[4] === null); // Will log 'false', because the slot does not contain the value 'null'
 console.log(4 in numbers); // Will log 'false', because the index '4' is not initialized

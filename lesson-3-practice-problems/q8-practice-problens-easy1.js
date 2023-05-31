f10a278efb552e02ac3289184e0c6e5e3f3c0e3f
Bobby <simplyhappydev@gmail.com>
2023-05-31 14:57:59 -0500

    Adding string to an Array

---
+++ lesson-3-practice-problems/q8-practice-problens-easy1.js
@@ -0,0 +1,12 @@
+/**
+Question 8
+How can we add the family pet, "Dino", to the following array?
+ */
+
+let flintstones = ['Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles'];
+
+//Use the push() method to add to the end of the array or the unshift() method to add to the begining. Or add in at a specific index
+
+flintstones.push('Dino');
+
+console.log(flintstones);

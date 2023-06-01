00661810eebbcc567cb48cd2d259ab1b091c72d0
Bobby <simplyhappydev@gmail.com>
2023-06-01 15:58:07 -0500

    using the spread operator to add in multiple items into an Array

---
+++ lesson-3-practice-problems/q9-practice-problens-easy1.js
@@ -0,0 +1,11 @@
+/*Question 9
+In the previous problem, our first answer added 'Dino' to the array like this: 
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones.push("Dino");
+
+How can we add multiple items to our array? ('Dino' and 'Hoppy')
+*/
+let flintstones = ['Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles'];
+flintstones = [...flintstones, 'Dino', 'Hoppy']; //using the <...> spread operator
+
+console.log(flintstones);

72985701cbc4dbbf9c5479cb88c4ae8140b66de3
Bobby <simplyhappydev@gmail.com>
2023-06-02 20:54:37 -0500

    Using .replace() method to replace string with a new string

---
+++ lesson-3-practice-problems/practice-problems-easy-2/q1-easy2.js
@@ -0,0 +1,11 @@
+/*
+Question 1
+Given a string, return a new string that replaces every occurrence of the word "important" with "urgent":
+*/
+let advice =
+  'Few things in life are as important as house training your pet dinosaur.';
+
+let urgentAdvice = advice.replace('important', 'urgent');
+
+console.log(advice);
+console.log(urgentAdvice);

333e76cfe30099c0ccc4b1f729145497f94cf174
Bobby <simplyhappydev@gmail.com>
2023-06-03 18:57:22 -0500

    Using the includes() method to check whether an element in an Array returning true if so, false if not

---
+++ lesson-3-practice-problems/practice-problems-easy-2/q3-easy2.js
@@ -0,0 +1,12 @@
+/*
+Question 3
+Given a number and an array, determine whether the number is included in the array.
+*/
+let numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
+
+let number1 = 8; // false
+let number2 = 95; // true
+
+console.log(`numbers.includes(number1) : ` + numbers.includes(number1));
+
+console.log(`numbers.includes(number2) : ` + numbers.includes(number2));

40ae611c4076229f2855d78d134e709148642f36
Bobby <simplyhappydev@gmail.com>
2023-06-16 16:18:05 -0500

    Adding in notes

--- lesson-3-practice-problems/practice-problems-easy-3/q3-easy3.js
+++ lesson-3-practice-problems/practice-problems-easy-3/q3-easy3.js
@@ -8,3 +8,7 @@ str2 = 'goodbye!'; //reassigns str2 to the String 'goodbye!'
 console.log(str1); //logs the current value that str1 is assigned to => 'hello there'
 
 console.log(str2); //logs the current value that str2 is assigned to => 'goodbye!'
+
+/**Notes:
+The output is hello there since we are dealing with strings. In JavaScript, strings are primitive values and are immutable; they can't be changed. That also means that JavaScript creates a new copy of the string when assigning a string to a variable. Thus, line 2 assigns str2 a new string that happens to be a copy of str1's value. Line 3, in turn, assigns str2 to an entirely new string.
+ */

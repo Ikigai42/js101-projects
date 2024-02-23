5d91921e28776e4f90079710cc0f9953f3ac1a5a
Bobby <simplyhappydev@gmail.com>
2024-02-22 20:10:54 -0600

    Add version 2 of the calculator assignment

---
+++ lesson-2-small-programs/walkthrough-calculator-v2.js
@@ -0,0 +1,5 @@
+// ask user for first number
+// ask user for second number
+// ask user for operation to perform on the two numbers
+// perform the opeartion the user inputs
+// display result of the operation
